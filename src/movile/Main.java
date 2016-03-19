ee89a0a9b665ad32783f8325f3cf480730dd4d0b
jabcross <pedrociambra@gmail.com>
2016-03-19 15:31:53 -0300

    StateMachine and Tree implemented

--- src/movile/Main.java
+++ src/movile/Main.java
@@ -9,6 +9,31 @@ public class Main {
 	static final String apibase = "192610098:AAEGaQN3juTYXBotgkRFxKbMP8FXRI9JAmw/";
 	public static void main(String[] args) {
 	
+		TreeNode root = new TreeNode("root","root",null,null);
+		
+		TreeNode cursor;
+
+		root.addChild("category","pizza",null);
+		cursor = root.getChild("pizza");
+		cursor.addChild("restaurant","Friend Brothers",null);
+		// Friend Brother's Pizza
+		
+		cursor = root.getChild("Friend Brother's Pizzeria");
+		cursor.addChild("meal", "Main course", null);
+		cursor.addChild("meal", "Drink", null);
+		cursor = cursor.getChild("Main Course");
+		cursor.addChild("food", "Cockatrice Pizza", 
+				new Item("Cockatrice Pizza","001",999999));
+		cursor.addChild("food", "Paperoni Pizza", 
+				new Item("Paperoni Pizza","002",0.5));
+		cursor = cursor.getFather().getChild("Drink");
+		cursor.addChild("food", "MtnDew", new Item("MtnDew", "012", 5));
+		
+		while(true){
+			
+
+		}
+		
 		JSONObject obj =  Client.doGet(urlbase+apibase+"getMe");
 		JSONObject obj2 = obj.getJSONObject("result");
 		System.out.println(obj2.get("first_name"));
